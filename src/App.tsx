691a9e0f3c49b45255c83f756ea410fb03dab574
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-10-16 09:51:49 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AccountType />} />
+        <Route path="/fillinfofirst" element={<FillInfoFirst />} />
+        <Route path="/fillinfosecond" element={<FillInfoSecond />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
