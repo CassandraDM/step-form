12f73cec969c6b7aefb936c91857fe6252613979
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-10-16 14:44:50 +0200

    tailwind + font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        zinc: {
+          350: "#C8C8D0",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
