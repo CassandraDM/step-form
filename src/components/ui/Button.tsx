7202c7852aa5bc8523725337a66f9f67c2ae5e9c
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-10-18 14:38:40 +0200

    fill presonnal info + some correction

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,13 +2,22 @@ type ButtonProps = {
   goTo?: () => void;
   goBackTo?: () => void;
   type: "next" | "previous";
+  onClick?: (e: React.FormEvent) => void;
 };
 
-const Button = ({ goTo, goBackTo, type }: ButtonProps) => {
+const Button = ({ goTo, goBackTo, type, onClick }: ButtonProps) => {
   if (type === "next") {
+    if (!goTo) {
+      throw new Error(
+        "The 'goTo' prop is required for the 'next' button type."
+      );
+    }
     return (
       <button
-        onClick={goTo}
+        onClick={(e) => {
+          if (onClick) onClick(e);
+          goTo();
+        }}
         className="flex h-10 px-4 justify-center items-center rounded bg-pastel-purple-500 text-white"
       >
         Next
